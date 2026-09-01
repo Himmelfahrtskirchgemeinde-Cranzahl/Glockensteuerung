95bade6beb4c0da72ec6e1ded880d6c212c06738
Josuadev <82457722+JosuaDev@users.noreply.github.com>
2026-09-01 00:49:14 +0000

    Warnbanner: 'Gateway nicht erreichbar' statt 'Automatik-Dienst'

--- extension/src/App.vue
+++ extension/src/App.vue
@@ -535,11 +535,11 @@ async function loadNextRingings() {
           <template v-else>
           <!-- ▸ Steuerung -->
           <template v-if="view === 'steuerung' && canView('steuerung')">
-            <!-- Automatik-Dienst weg? Dann laeutet nichts von allein - das muss man sehen. -->
+            <!-- Gateway weg? Dann laeutet nichts von allein - das muss man sehen. -->
             <div v-if="gatewayDown" class="gs-banner gw-down">
               <svg viewBox="0 0 24 24" width="20" height="20" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" style="flex:none"><path d="M12 9v4M12 17h.01"/><path d="M10.3 3.9 1.8 18a2 2 0 0 0 1.7 3h17a2 2 0 0 0 1.7-3L13.7 3.9a2 2 0 0 0-3.4 0Z"/></svg>
               <div class="txt">
-                <b>Automatik-Dienst nicht erreichbar</b><br>
+                <b>Gateway nicht erreichbar</b><br>
                 <small>Es wird zurzeit <b>nicht automatisch geläutet</b>. Manuelles Läuten über die Knöpfe funktioniert weiterhin. {{ gatewayDownText }}</small>
               </div>
             </div>
